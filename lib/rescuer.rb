5c2d1269dd6d61ed777121f0d8652a3d134fa50a
Marc Siegel <marc@usainnov.com>
2014-04-17 10:05:52 -0400

    Clarify name as exceptions_to_rescue

--- lib/rescuer.rb
+++ lib/rescuer.rb
@@ -1,14 +1,14 @@
 require 'rescuer/version'
 
 module Rescuer
-  DEFAULT_EXCEPTIONS = [StandardError]
+  DEFAULT_EXCEPTIONS_TO_RESCUE = [StandardError]
 
-  def new(*exceptions)
-    exceptions = DEFAULT_EXCEPTIONS if exceptions.nil? || exceptions.empty?
+  def new(*exceptions_to_rescue)
+    exceptions_to_rescue = DEFAULT_EXCEPTIONS_TO_RESCUE if exceptions_to_rescue.nil? || exceptions_to_rescue.empty?
     raise ArgumentError, 'no block given' unless block_given?
     begin
       Success.new(yield)
-	  rescue *exceptions => ex
+	  rescue *exceptions_to_rescue => ex
       Failure.new(ex)
     end
   end
